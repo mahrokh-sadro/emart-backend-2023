f9ff2d743c85fc8da18861761c25d520ad9c436b
mahrokh sadro <msadrolodabaee@myseneca.ca>
2023-01-19 21:52:02 -0500

    first commit

---
+++ services/CustomerService.js
@@ -0,0 +1,7 @@
+const CustomerModel = require("../models/CustomerModel.js");
+
+exports.userById = (req, res, next, id) => {};
+
+exports.read = (req, res) => {};
+
+exports.update = (req, res) => {};
